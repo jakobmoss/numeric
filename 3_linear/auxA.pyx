d475f606e21fea35dbc60e904654189a384fd799
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-04-24 19:44:37 +0200

    2a: qr-decomp

--- 3_linear/auxA.pyx
+++ 3_linear/auxA.pyx
@@ -1,1 +1,28 @@
 # cython: language_level=3
+
+import numpy as np
+
+
+#
+# Function to calculate the QR-decomposition
+#
+def decomp(Q, R):
+    """
+    Calculates the QR-decomposition in-place (i.e. the original matrix is 
+    replaced) using stabilized Gram-Schmidt.
+
+    Arguments:
+    - `Q`: Matrix of dimension (n x m) to be transformed <type: double>
+    - `R`: Empty matrix of dimension (m x m) <type: double>
+    """
+    # Loop over number of columns
+    for i in range(Q.shape[1]):
+        qi = Q[:, i]  # Pointer to the i'th column
+        R[i, i] = np.sqrt(np.dot(qi, qi))  # Fill R_ii
+        qi /= R[i, i]  # Scale by 1/R_ii
+
+        # Normalize to all columns to the right
+        for j in range(i+1, Q.shape[1]):
+            qj = Q[:, j]  # Pointer to the j'th column
+            R[i, j] = np.dot(qi, qj)  # Fill R_ij
+            qj -= qi*R[i, j]  # Do the normalization
--- 3_linear/mainA.pyx
+++ 3_linear/mainA.pyx
@@ -1,7 +1,9 @@
 # cython: language_level=3
 
+import numpy as np
 import auxA as qr
 
+        
 #
 # Main function
 #
@@ -10,4 +12,22 @@ def mainA():
     Test of the routines for solving linear equations
     """
 
-    print('hej!')
+    # Double precision test-matrix
+    A = np.array([[-4, -4, 2], [0, 2, 4], [-3, -2, 3]], dtype='float64')
+#    A = np.array([[4, -6, 3], [3, -5, 8], [5, 4, -7]], dtype='float64')
+    R = np.zeros((A.shape[1], A.shape[1]), dtype='float64')
+    
+    print(' -- Before GS --')
+    print('A =')
+    print(A)
+    print('R =')
+    print(R)
+
+    # Do the decomposition
+    qr.decomp(A, R)
+    
+    print('\n -- After GS --')
+    print('A =')
+    print(A)
+    print('R =')
+    print(R)
