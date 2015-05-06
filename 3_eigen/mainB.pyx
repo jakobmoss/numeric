edeb9226b01e1d6a9cd9b89af1d18231c2c8daa8
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-06 16:16:57 +0200

    Initial commit of 3b

---
+++ 3_eigen/mainB.pyx
@@ -0,0 +1,14 @@
+# cython: language_level=3
+
+import numpy as np
+import auxB as jacobi
+
+
+#
+# Main function
+#
+def mainB():
+    """
+    Test of the routines for diagonalization
+    """
+    
