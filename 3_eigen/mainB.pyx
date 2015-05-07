8c336e903010195e8cf1c5fb597389f9924e321c
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-07 11:44:45 +0200

    3b: Test of all routines

--- 3_eigen/mainB.pyx
+++ 3_eigen/mainB.pyx
@@ -5,22 +5,16 @@ import auxB as jacobi
 
 
 #
-# Main function
+# Testing routines
 #
-def mainB():
+def test_cyclic(A, d, V):
     """
-    Test of the routines for diagonalization
+    Testing cyclic Jacobi routine
     """
-        # Initialization of test matrices
-#    A = np.array([[3, 1, -1], [1, 3, -1], [-1, -1, 5]], dtype='float64')
-    A = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2],
-                  [10, 9, 2, 3]], dtype='float64')
     Aorg = np.copy(A)
-    d = np.zeros(A.shape[0], dtype='float64')
-    V = np.zeros(A.shape, dtype='float64')
-
+    
     # Run the test
-    print(' -- Testing Jacobi diagonalization -- ')
+    print(' -- Testing cyclic Jacobi diagonalization -- ')
     print('A = \n', A)
     rot = jacobi.diag_cyclic(A, d, V)
 
@@ -43,3 +37,97 @@ def mainB():
 
     print('\nCheck of diagonalization:\nV^T A V =')
     print(np.dot(np.dot(np.transpose(V), Aorg), V))
+
+    
+def test_row(A, d, V):
+    """
+    Testing eigenvalue-by-eigenvalue Jacobi routine
+    """
+    Aorg = np.copy(A)
+    
+    # Run the test
+    print('\n\n -- Testing row-by-row Jacobi diagonalization -- ')
+    print('A = \n', A)
+    rot = jacobi.diag_eig(A, d, V)
+
+    print('\nNumber of rotations used = ', rot)
+    print('\nEigenvalues of A :\nd = \n', d)
+    print('\nEigenvectors of A:\nV = \n', V)
+
+    print('\nOrthogonality of eigenvectors:')
+    for i in range(len(A)):
+        for j in range(len(A)):
+            forprint = 'V_' + str(i) + ' * V_' + str(j) + ' ='
+            print(forprint, np.dot(V[:,i], V[:,j]))
+
+    print('\nCheck of eigenvalues and -vectors:')
+    for i in range(len(A)):
+        forprint1 = 'A * V_' + str(i) + ' =\n'
+        forprint2 = 'd_' + str(i) + ' * V_' + str(i) + '=\n'
+        print(forprint1, np.dot(Aorg, V[:, i]))
+        print(forprint2, np.dot(d[i], V[:, i]))
+
+    print('\nCheck of diagonalization:\nV^T A V =')
+    print(np.dot(np.dot(np.transpose(V), Aorg), V))
+
+
+def test_great(A, d, V):
+    """
+    Testing 'row-by-row with greatest element first' Jacobi routines
+    """
+    Aorg = np.copy(A)
+    
+    # Run the test
+    print('\n\n -- Testing greatst element Jacobi diagonalization -- ')
+    print('A = \n', A)
+    rot = jacobi.diag_eig2(A, d, V)
+
+    print('\nNumber of rotations used = ', rot)
+    print('\nEigenvalues of A :\nd = \n', d)
+    print('\nEigenvectors of A:\nV = \n', V)
+
+    print('\nOrthogonality of eigenvectors:')
+    for i in range(len(A)):
+        for j in range(len(A)):
+            forprint = 'V_' + str(i) + ' * V_' + str(j) + ' ='
+            print(forprint, np.dot(V[:,i], V[:,j]))
+
+    print('\nCheck of eigenvalues and -vectors:')
+    for i in range(len(A)):
+        forprint1 = 'A * V_' + str(i) + ' =\n'
+        forprint2 = 'd_' + str(i) + ' * V_' + str(i) + '=\n'
+        print(forprint1, np.dot(Aorg, V[:, i]))
+        print(forprint2, np.dot(d[i], V[:, i]))
+
+    print('\nCheck of diagonalization:\nV^T A V =')
+    print(np.dot(np.dot(np.transpose(V), Aorg), V))
+
+    
+#
+# Main function
+#
+def mainB():
+    """
+    Test of the routines for diagonalization
+    """
+
+    # Test the cyclic algorithm
+    A1 = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2],
+                  [10, 9, 2, 3]], dtype='float64')
+    d1 = np.zeros(A1.shape[0], dtype='float64')
+    V1 = np.zeros(A1.shape, dtype='float64')
+    test_cyclic(A1, d1, V1)
+
+    # Test the row-by-row algorithm
+    A2 = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2],
+                  [10, 9, 2, 3]], dtype='float64')
+    d2 = np.zeros(A2.shape[0], dtype='float64')
+    V2 = np.zeros(A2.shape, dtype='float64')
+    test_cyclic(A2, d2, V2)
+
+    # Test the greatest element algorithm
+    A3 = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2],
+                  [10, 9, 2, 3]], dtype='float64')
+    d3 = np.zeros(A3.shape[0], dtype='float64')
+    V3 = np.zeros(A3.shape, dtype='float64')
+    test_cyclic(A3, d3, V3)
