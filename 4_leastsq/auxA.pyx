84ce16f1a6895255434c31614275119f10667861
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-07 21:06:49 +0200

    4a: Finished writing fitting routine. Not tested.

--- 4_leastsq/auxA.pyx
+++ 4_leastsq/auxA.pyx
@@ -13,26 +13,55 @@ import givens as qr
 #
 def qrfit(flist, x, y, dy):
     """
-    
+    Calculates the fit of the form \sum{c_i * f_i(x)} to the data 
+    (x, y) with error dy on y, using least mean squares and 
+    QR-decomposition with Given's rotation.
+
+    Returns the fitting coefficients as a vector and the covariance
+    matrix.
+
     Arguments:
-    - `flist`:
-    - `x`:
-    - `y`:
-    - `dy`:
+    - `flist`: List of functions to fit to the data
+    - `x`: X-data [vector]
+    - `y`: Y-data [vector]
+    - `dy`: Error on y-data [vector]
     """
     # Initializations
     n = len(x)
     m = len(flist)
     A = np.zeros((n, m), dtype='float64')
-    b = np.zeros(n, dtype='float64')
+    c = np.zeros(n, dtype='float64')
 
-    # Fill A and b  -->  Loop over all data points
+    # Fill A and c  -->  Loop over all data points
     for i in range(n):
-        b[i] = y[i] / dy[i]  # Weighting by error
+        c[i] = y[i] / dy[i]  # Weighting by error
 
         # Loop over fitting functions
         for j in range(m):
             A[i, j] = flist[j](x[i]) / dy[i]
 
-    # Decompose using Given's rotation
+    # Decompose using Given's rotation and solve by in-place backsub
     qr.decomp(A)
+    qr.solve(A, c)
+
+    # Explictly build the R-matrix and find the inverse
+    Rinv = np.zeros((n, n), dtype='float64')
+    qr.inverse(qr.build_R(A), Rinv)
+
+    # Calculate the covariance matrix S
+    S = np.dot(Rinv, np.transpose(Rinv))
+
+    # Return the fitting coefficients and the covariance matrix
+    return c, S
+
+#
+# Function to evaluate a fit
+#
+def eval_fit(flist, c, x):
+    """
+    Evaluates a fit of the form \sum{c_i * f_i(x)} at point x.
+
+    Arguments: See qrfit
+    """
+    return sum([c[i] * flist[i](x) for i in range(len(c))])
+    
