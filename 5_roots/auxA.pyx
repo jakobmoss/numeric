8ed861ec09b4e727a9c55eb58a726765c2fb585c
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-12 15:38:51 +0200

    5a: The algorithm seems to work...

--- 5_roots/auxA.pyx
+++ 5_roots/auxA.pyx
@@ -7,3 +7,74 @@ import sys
 
 # QR-decomposition routines using Given's rotation
 import givens as qr
+
+
+#
+# Function to find the root of a function with Newton's method
+#
+def newton(f, x0, dx, eps):
+    """
+    Finds the root of a function using Newton's method with simple
+    backtracking linesearch and numerical evaluation of the Jacobian. Uses 
+    Given's rotation for QR-decomposition.
+
+    Returns the approximation of the root.
+
+
+    Arguments:
+    - `f`: Function f(x) to find the root of. Argument x is a vector.
+    - `x0`: Starting point as a vector
+    - `dx`: The deltaX to be used in evaluation of the Jacobian [vector]
+    - `eps`: Desired accuracy 
+    """
+    # Initializations
+    x = np.copy(x0)
+    n = len(x)
+    J = np.zeros((n, n), dtype='float64')
+
+    # BEGIN ROOT SEARCH  -->  Keep going until accuracy reached
+    while True:
+        fx = f(x)
+
+        # BEGIN TO FILL JACOBIAN -> J  -->  Numerical estimate
+        for j in range(n):
+            x[j] += dx[j]
+            df = f(x) - fx
+
+            for i in range(n):
+                J[i, j] = df[i] / dx[j]
+
+            x[j] -= dx[j]
+        # END JACOBIAN
+
+        # Decompose and solve using Given's rotation
+        qr.decomp(J)
+        Dx = -fx
+        qr.solve(J, Dx)
+
+        # BEGIN BACKTRACKING LINESEARCH
+        lamb = 2.0  # lambda is a special keyword
+        while True:
+            lamb /= 2
+            y = x + Dx*lamb
+            fy = f(y)
+
+            # Condition to end  --  Note: norm(x) := np.sqrt(np.dot(x,x))
+            if (np.sqrt(np.dot(fy, fy)) < (1 - lamb/2)*np.sqrt(np.dot(fx, fx))) \
+               or (lamb < 1/128):
+                break
+        # END BACKTRACK
+
+        # Store latest approximation
+        x = y
+        fx = fy
+
+        # Condition to end
+        if (np.sqrt(np.dot(Dx, Dx)) < np.sqrt(np.dot(dx, dx))) \
+           or (np.sqrt(np.dot(fx, fx)) < eps):
+            break
+        
+    # END ROOT SEARCH
+
+    # Return the accepted approximation
+    return x
--- 5_roots/mainA.pyx
+++ 5_roots/mainA.pyx
@@ -3,6 +3,20 @@
 import numpy as np
 import auxA as root
 
+#
+# Rosenbrock's valley function 
+#
+def rosen(q):
+    # Init
+    z = np.zeros(2, dtype='float64')
+    x = q[0]
+    y = q[1]
+
+    # Calculate and return
+    z[0] = 2*(1-x)*(-1) + 100*2*(y-x*x)*(-1)*2*x 
+    z[1] = 100*2*(y-x*x)
+    return z
+
 
 #
 # Main function
@@ -11,4 +25,16 @@ def mainA():
     """
     Test of the root finding methods
     """
-    print('hej')
+    ## PART TWO  -->  Minimization of the Rosenbrock valley function
+    print('\n -- Root search of the Rosenbrock valley function --')
+
+    # Initialization
+    x0 = np.array([5, 10], dtype='float64')
+    dx = np.array([1e-7, 1e-7], dtype='float64')
+    print('\nStarting point:\nx0 =', x0, '\nf(x0) =', rosen(x0))
+
+    # Run the root search
+    roots = root.newton(rosen, x0, dx, 1e-6)
+    print('\nSolution:\nx =', roots, '\nf(x) =', rosen(roots))
+    
+    
