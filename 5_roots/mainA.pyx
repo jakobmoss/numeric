db2e62a4e1d02e9afc3bf58ed309265a18a2f03a
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-12 15:59:15 +0200

    5a: Now with a counter of function calls

--- 5_roots/mainA.pyx
+++ 5_roots/mainA.pyx
@@ -4,9 +4,13 @@ import numpy as np
 import auxA as root
 
 #
-# Rosenbrock's valley function 
+# The Rosenbrock valley function (or actually the gradient of it)
 #
 def rosen(q):
+    # Global variable to count the number of calls to the function
+    global ncalls
+    ncalls += 1
+        
     # Init
     z = np.zeros(2, dtype='float64')
     x = q[0]
@@ -25,10 +29,15 @@ def mainA():
     """
     Test of the root finding methods
     """
-    ## PART TWO  -->  Minimization of the Rosenbrock valley function
+    # Global variable to count the number of function calls
+
+    ## PART TWO  -->  Root search of the Rosenbrock valley function by running
+    ##                Newton's method on the gradient
     print('\n -- Root search of the Rosenbrock valley function --')
 
     # Initialization
+    global ncalls
+    ncalls = 0
     x0 = np.array([5, 10], dtype='float64')
     dx = np.array([1e-7, 1e-7], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', rosen(x0))
@@ -36,5 +45,6 @@ def mainA():
     # Run the root search
     roots = root.newton(rosen, x0, dx, 1e-6)
     print('\nSolution:\nx =', roots, '\nf(x) =', rosen(roots))
+    print('\nNumber of calls to the function:\nn =', ncalls)
     
     
