8c3e2b78dd967d029d2488e31616351b54eb7edd
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-17 15:36:35 +0200

    Moved the reset of ncalls to be consistent

--- 5_roots/mainA.pyx
+++ 5_roots/mainA.pyx
@@ -24,12 +24,12 @@ def mainA():
     print('\nSystem: Axy=1 ;  exp(-x)+exp(-y)=1 + 1/A')
 
     # Initialization
-    globvar.ncalls = 0
     x0 = np.array([3, -1], dtype='float64')
     dx = np.array([1e-9, 1e-9], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', eqsys(x0))
 
     # Run the root search
+    globvar.ncalls = 0
     roots1 = root.newton(eqsys, x0, dx, 1e-12)
     print('\nSolution:\nx =', roots1, '\nf(x) =', eqsys(roots1))
     print('\nNumber of calls to the function:\nn =', globvar.ncalls)
@@ -41,12 +41,12 @@ def mainA():
     print('\nFunction: f(x,y) = (1 - x)^2 - 100*(y - x^2)^2')
 
     # Initialization
-    globvar.ncalls = 0
     x0 = np.array([-1, 5], dtype='float64')
     dx = np.array([1e-7, 1e-7], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', rosen(x0))
 
     # Run the root search
+    globvar.ncalls = 0
     roots2 = root.newton(rosen, x0, dx, 1e-8)
     print('\nSolution:\nx =', roots2, '\nf(x) =', rosen(roots2))
     print('\nNumber of calls to the function:\nn =', globvar.ncalls)
@@ -58,12 +58,12 @@ def mainA():
     print('\nFunction: f(x,y) = (x^2 + y - 11)^2 + (x + y^2 - 7)^2')
 
     # Initialization
-    globvar.ncalls = 0
     x0 = np.array([1, -1], dtype='float64')
     dx = np.array([1e-9, 1e-9], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', himmel(x0))
 
     # Run the root search
+    globvar.ncalls = 0
     roots3 = root.newton(himmel, x0, dx, 1e-12)
     print('\nSolution:\nx =', roots3, '\nf(x) =', himmel(roots3))
     print('\nNumber of calls to the function:\nn =', globvar.ncalls)
@@ -75,12 +75,12 @@ def mainA():
     print('\nFunction: f(x,y) = 0.26*(x^2 + y^2) - 0.48*xy')
 
     # Initialization
-    globvar.ncalls = 0
     x0 = np.array([2, -7], dtype='float64')
     dx = np.array([1e-9, 1e-9], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', matya(x0))
 
     # Run the root search
+    globvar.ncalls = 0
     roots4 = root.newton(matya, x0, dx, 1e-12)
     print('\nSolution:\nx =', roots4, '\nf(x) =', matya(roots4))
     print('\nNumber of calls to the function:\nn =', globvar.ncalls)
