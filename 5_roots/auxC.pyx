e6e457ceee9c76cc920c462a3af26ab6a63aa9b3
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-05-18 13:49:17 +0200

    5c: Done! But shows no improvement over simple line search?

--- 5_roots/auxC.pyx
+++ 5_roots/auxC.pyx
@@ -80,67 +80,6 @@ def newton(f, x0, dx, eps):
 
 
 #
-# Newton's method with user-supplied derivatives
-#
-def newton_deriv(f, x0, Jf, eps):
-    """
-    Finds the root of a function using Newton's method with simple
-    backtracking linesearch and a user-supplied Jacobian. Uses Given's rotation
-    for QR-decomposition.
-
-    Returns the approximation of the root.
-
-    Arguments:
-    - `f`: Function f(x) to find the root of. Argument x is a vector.
-    - `x0`: Starting point as a vector
-    - `Jf`: Function to calculate the Jacobian of the function f(x)
-    - `eps`: Desired accuracy 
-    """
-    # Initializations
-    x = np.copy(x0)
-    n = len(x)
-    J = np.zeros((n, n), dtype='float64')
-
-    # BEGIN ROOT SEARCH  -->  Keep going until accuracy reached
-    while True:
-        fx = f(x)
-
-        # Calculate Jacobian
-        J = Jf(x)
-
-        # Decompose and solve using Given's rotation
-        qr.decomp(J)
-        Dx = -fx
-        qr.solve(J, Dx)
-
-        # BEGIN BACKTRACKING LINESEARCH
-        lamb = 2.0  # lambda is a special keyword
-        while True:
-            lamb /= 2
-            y = x + Dx*lamb
-            fy = f(y)
-
-            # Condition to end  --  Note: norm(x) := np.sqrt(np.dot(x,x))
-            if (np.sqrt(np.dot(fy, fy)) < (1 - lamb/2)*np.sqrt(np.dot(fx, fx))) \
-               or (lamb < 1/128):
-                break
-        # END BACKTRACK
-
-        # Store latest approximation
-        x = y
-        fx = fy
-
-        # Condition to end
-        if np.sqrt(np.dot(fx, fx)) < eps:
-            break
-        
-    # END ROOT SEARCH
-
-    # Return the accepted approximation
-    return x
-
-
-#
 # Newton with better linesearch
 #
 def newton_quad(f, x0, dx, eps):
@@ -193,10 +132,10 @@ def newton_quad(f, x0, dx, eps):
 
         # Known values of the function to minimize [6.9]
         g0 = 0.5*nfx*nfx  # g(0)
-        gp0 = -nfx*nfx    # g'(0)
+        gp0 = (-1)*nfx*nfx    # g'(0)
 
         # Only enter if the step is rejected
-        while (nfy > (1 - lamb/2)*nfx) and (lamb > 1/128):
+        while (nfy > (1 - lamb/2)*nfx) and (lamb > 1.0/128):
 
             # Calculate approximation [6.11]
             gl = 0.5*nfy*nfy  # g(\lambda)
--- 5_roots/mainC.pyx
+++ 5_roots/mainC.pyx
@@ -31,7 +31,7 @@ def mainC():
     print('\nSystem: Axy=1 ;  exp(-x)+exp(-y)=1 + 1/A')
 
     # Initialization
-    x0 = np.array([3, -1], dtype='float64')
+    x0 = np.array([3, 2], dtype='float64')
     dx = np.array([1e-9, 1e-9], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', eqsys(x0))
 
@@ -55,12 +55,12 @@ def mainC():
     print('\nFunction: f(x,y) = (1 - x)^2 - 100*(y - x^2)^2')
 
     # Initialization
-    x0 = np.array([-1, 5], dtype='float64')
+    x0 = np.array([0, 2], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', eqsys(x0))
+    dx = np.array([1e-7, 1e-7], dtype='float64')
 
     # Run the root search with numerical derivatives
     globvar.ncalls = 0
-    dx = np.array([1e-7, 1e-7], dtype='float64')
     roots1 = root.newton(rosen, x0, dx, 1e-8)
     print('\nSolution (simple):\nx =', roots1, '\nf(x) =', rosen(roots1))
     print('Calls to the function: ', globvar.ncalls)
@@ -79,20 +79,20 @@ def mainC():
     print('\nFunction: f(x,y) = (x^2 + y - 11)^2 + (x + y^2 - 7)^2')
 
     # Initialization
-    x0 = np.array([1, -1], dtype='float64')
+    x0 = np.array([4, 3], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', eqsys(x0))
+    dx = np.array([1e-9, 1e-9], dtype='float64')
 
     # Run the root search with numerical derivatives
     globvar.ncalls = 0
-    dx = np.array([1e-9, 1e-9], dtype='float64')
     roots1 = root.newton(himmel, x0, dx, 1e-12)
-    print('\nSolution (numerical):\nx =', roots1, '\nf(x) =', himmel(roots1))
+    print('\nSolution (simple):\nx =', roots1, '\nf(x) =', himmel(roots1))
     print('Calls to the function: ', globvar.ncalls)
 
     # Run the root search with user-supplied derivatives
     globvar.ncalls = 0
-    roots2 = root.newton_deriv(himmel, x0, diff_himmel, 1e-12)
-    print('\nSolution (user-sup):\nx =', roots2, '\nf(x) =', himmel(roots2))
+    roots2 = root.newton_quad(himmel, x0, dx, 1e-12)
+    print('\nSolution (quad):\nx =', roots2, '\nf(x) =', himmel(roots2))
     print('Calls to the function: ', globvar.ncalls)
 
 
@@ -103,18 +103,18 @@ def mainC():
     print('\nFunction: f(x,y) = 0.26*(x^2 + y^2) - 0.48*xy')
 
     # Initialization
-    x0 = np.array([2, -7], dtype='float64')
+    x0 = np.array([-0.5, 0.5], dtype='float64')
     print('\nStarting point:\nx0 =', x0, '\nf(x0) =', eqsys(x0))
+    dx = np.array([1e-9, 1e-9], dtype='float64')
 
     # Run the root search with numerical derivatives
     globvar.ncalls = 0
-    dx = np.array([1e-9, 1e-9], dtype='float64')
     roots1 = root.newton(matya, x0, dx, 1e-12)
-    print('\nSolution (numerical):\nx =', roots1, '\nf(x) =', matya(roots1))
+    print('\nSolution (simple):\nx =', roots1, '\nf(x) =', matya(roots1))
     print('Calls to the function: ', globvar.ncalls)
 
     # Run the root search with user-supplied derivatives
     globvar.ncalls = 0
-    roots2 = root.newton_deriv(matya, x0, diff_matya, 1e-12)
-    print('\nSolution (user-sup):\nx =', roots2, '\nf(x) =', matya(roots2))
+    roots2 = root.newton_quad(matya, x0, dx, 1e-12)
+    print('\nSolution (quad):\nx =', roots2, '\nf(x) =', matya(roots2))
     print('Calls to the function: ', globvar.ncalls)
