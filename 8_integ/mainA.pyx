c2149e32017dacfff84da0c7df767a0b4908c5f2
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-06-27 20:16:52 +0200

    Initial commit of 8

---
+++ 8_integ/mainA.pyx
@@ -0,0 +1,14 @@
+# cython: language_level=3
+
+import numpy as np
+import auxA as adint
+import sys
+
+
+#
+# Main function
+#
+def mainA():
+    """
+    Test of the ODE routines
+    """
---
+++ 8_integ/setupA.py
@@ -0,0 +1,13 @@
+# Needed to build Cython
+from distutils.core import setup
+from Cython.Build import cythonize
+
+# Change to newest gcc on Darwin
+import os
+from sys import platform
+if platform.lower() == 'darwin':
+    os.environ['CC'] = 'gcc-5'
+
+# Do the build
+setup(ext_modules=cythonize('auxA.pyx'))
+setup(ext_modules=cythonize('mainA.pyx'))
