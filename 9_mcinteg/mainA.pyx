17e540c0ca9b13d0fa0c14add67bed46bf638dab
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-06-28 17:46:44 +0200

    9a: Now with (unfinished) error-estimate

--- 9_mcinteg/mainA.pyx
+++ 9_mcinteg/mainA.pyx
@@ -13,7 +13,7 @@ def f1(q):
     """
     y * cos(x)
     """
-    return q[1] * math.cos(q[0])   
+    return math.cos(q[0]) * math.sin(q[1])
 
 
 #
@@ -28,12 +28,12 @@ def mainA():
     ##############
     # Initializations
     a1 = np.array([-math.pi/2.0, 0])
-    b1 = np.array([math.pi/2.0, 1])
-    exact1 = 1
+    b1 = np.array([math.pi/2.0, math.pi])
+    exact1 = 4
     N1 = 1000
 
     # Pretty print!
-    print('\n ** Integrating y*cos(x) from (x,y) = (-pi/2,0) to (pi/2,1) ** ')
+    print('\n ** Integrating cos(x)*sin(y) from (x,y) = (-pi/2,0) to (pi/2,pi) ** ')
     print('Exact solution = {0:d}'.format(exact1))
 
     # Perform the integration
@@ -43,3 +43,13 @@ def mainA():
     print('Integral       = {0:.15f}'.format(res1))
     print('Error estimate = {0:.5e}'.format(err1))
     print('Actual error   = {0:.5e}'.format(abs(res1 -  exact1)))
+    print('\nA plot of how the error scales with N is shown in plot.A.pdf',
+          '(the data is kept in Aerr.dat)')
+
+    # Error-estimation as a function of N
+    #  .. it is almost abusive to use stderr for this..
+    print('# Error-estimate from part A', file=sys.stderr)
+    for k in range(1, 6):
+        N = int(math.pow(10, k))
+        res, err = mcint.plainmc(f1, a1, b1, N)
+        print('{0:12d} {1:16.12f}'.format(N, err), file=sys.stderr)
