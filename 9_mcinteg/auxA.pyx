e7f0b190d645c4e6f3618ffb3ae22721fcc79071
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-06-28 17:03:35 +0200

    Draft of plain MC

--- 9_mcinteg/auxA.pyx
+++ 9_mcinteg/auxA.pyx
@@ -4,3 +4,51 @@
 import numpy as np
 import math
 import sys
+
+#
+# Plain Monte Carlo integration
+#
+def plainmc(F, a, b, N):
+    """
+    
+    Arguments:
+    - `F`: Function to integrate
+    - `a`: Vector of stating point
+    - `b`: Vector of end point
+    - `N`: Number of points to sample
+    """
+    # Initilizations
+    volume = 1
+    x      = np.zeros(a.shape, dtype='float')
+    s      = 0  # Sum
+    ss     = 0  # Sum of Squares
+    
+    # Calculate volume
+    for i in range(len(a)):
+        volume *= b[i] - a[i]
+
+    # Sample the N points
+    for i in range(N):
+        randsamp(a, b, x)
+        y = F(x)
+        s += y
+        ss += y*y
+
+    # Calculate average and sigma=sqrt(variance)
+    mean  = s/N
+    sigma = math.sqrt(ss / N - mean*mean)
+
+    # Return estimate of result and error
+    err = volume * sigma/math.sqrt(N)
+    res = volume * mean
+    return res, err
+
+    
+#
+# Random sampling for the Monte Carlo integration
+#
+def randsamp(a, b, x):
+    """
+    Fill vector x with random numbers on the interval [a, b]
+    """
+    x = a + np.random.uniform(0, 1, len(a)) * (b - a)
--- 9_mcinteg/mainA.pyx
+++ 9_mcinteg/mainA.pyx
@@ -6,6 +6,15 @@ import math
 import auxA as mcint
 import sys
 
+#
+# Test-functions to integrate
+#
+def f1(q):
+    """
+    y * cos(x)
+    """
+    return q[1] * math.cos(q[0])   
+
 
 #
 # Main function
@@ -14,4 +23,10 @@ def mainA():
     """
     Test of the Monte Carlo routines
     """
-    
+    ##############
+    # Function 1 #
+    ##############
+    # Initializations
+    a = np.array([-math.pi/2.0, 0])
+    b = np.array([math.pi/2.0, 1])
+    exact1 = 1
