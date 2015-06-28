70770547d3192c7137c63380de66f380f00b981b
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-06-28 10:00:48 +0200

    8a seems to work

--- 8_integ/auxA.pyx
+++ 8_integ/auxA.pyx
@@ -12,7 +12,7 @@ def rcquad(F, a, b, acc, eps):
     """
     Recursive adaptive integration using Closed QUADratures. Uses a 4th order
     trapezium rule for evaluation with 2nd order rectangular rule for error 
-    estimation.
+    estimation. Re-uses points to minimize number of evaluations.
 
     Returns the integral of F(x) from a to b, and an estimate of the error.
     
@@ -34,7 +34,8 @@ def rcquad(F, a, b, acc, eps):
         # Call the integrator and return
         Q, err = rcquad24(F, a, b, f1, f4, acc, eps, 0)
         return Q, err
-
+    else:
+        print('Check your limits!', file=sys.stderr)
         
 #
 # CLOSED QUAD  -->  Integrator
@@ -67,7 +68,7 @@ def rcquad24(F, a, b, f1, f4, acc, eps, nrec):
     w1 = 1/8.0; w2 = 3/8.0; w3 = 3/8.0; w4 = 1/8.0
 
     # Weights of the 2nd order rectangular rule
-    v1 = 1/2.0; v2 = 1/2.0; v3 = 1/2.0; v4 = 1/2.0
+    v1 = 1/4.0; v2 = 1/4.0; v3 = 1/4.0; v4 = 1/4.0
 
     # Calculate the quadratures of higher order Q and lower order q
     Q = h * (w1*f1 + w2*f2 + w3*f3 + w4*f4)
@@ -81,11 +82,102 @@ def rcquad24(F, a, b, f1, f4, acc, eps, nrec):
     if err < tol:
         return Q, err
     #
-    # If not: Split the interval in three and integrate each seperately with
+    # If not: Split the interval in three and integrate each seperately with a
     # scaled absolute accuracy goal. Appropriate points are re-used.
     else:
         accscale = math.sqrt(3)
         Ql, errl = rcquad24(F, a, a+h/3.0, f1, f2, acc/accscale, eps, nrec+1)
         Qm, errm = rcquad24(F, a+h/3.0, a+2*h/3.0, f2, f3, acc/accscale, eps, nrec+1)
         Qr, errr = rcquad24(F, a+2*h/3.0, b, f3, f4, acc/accscale, eps, nrec+1)
-        return (Ql + Qm + Qr), (errl + errm + errr)
+        Qtot = Ql + Qm + Qr
+        errtot = math.sqrt(errl*errl + errm*errm + errr*errr)
+        return Qtot, errtot
+
+
+    
+#
+# OPEN QUAD  -->  Interface
+#
+def roquad(F, a, b, acc, eps):
+    """
+    Recursive adaptive integration using Open QUADratures. Uses a 4th order
+    trapezium rule for evaluation with 2nd order rectangular rule for error 
+    estimation. Re-uses points to minimize number of evaluations.
+
+    Returns the integral of F(x) from a to b, and an estimate of the error.
+    
+    Arguments:
+    - `F`: Function to integrate
+    - `a`: Stating point
+    - `b`: End point
+    - `acc`: Allowed absolute error
+    - `eps`: Allowed relative error
+    """
+    # If the limits are okay: Perform the integration
+    if a < b:
+        # Pre-calculate the end-points (re-usable)
+        h = b - a
+        x2 = 1/3.0; x3 = 2/3.0  # From the points 2/6 and 4/6
+        f2 = F(a + h*x2)
+        f3 = F(a + h*x3)
+
+        # Call the integrator and return
+        Q, err = roquad24(F, a, b, f2, f3, acc, eps, 0)
+        return Q, err
+    else:
+        print('Check your limits!', file=sys.stderr)
+        
+#
+# OPEN QUAD  -->  Integrator
+#
+def roquad24(F, a, b, f2, f3, acc, eps, nrec):
+    """
+    The actual integrator. Is called from the interface function rcquad.
+
+    Integrates the function F from a to b, re-using previously estimated 
+    points. If the error is to big, the interval is sub-divided and the 
+    integrator calls itself (hence, recursive).
+    
+    Specific arguments (re-usable points):
+    - `f2`: Estimated value at x2
+    - `f3`: Estimated value at x3
+    - `nrec`: The current level of recursion
+    """
+    # Report an error if the recursion gets to deep
+    if nrec > 15000:
+        print('\nroquad24: To many recursions!', file=sys.stderr)
+        return 0
+
+    # Estimate of the two remaining points
+    h = b - a
+    x1 = 1/6.0; x4 = 5/6.0
+    f1 = F(a + h*x1)
+    f4 = F(a + h*x4)
+
+    # Weights of the 4th order trapezium rule
+    w1 = 2/6.0; w2 = 1/6.0; w3 = 1/6.0; w4 = 2/6.0
+
+    # Weights of the 2nd order rectangular rule
+    v1 = 1/4.0; v2 = 1/4.0; v3 = 1/4.0; v4 = 1/4.0
+
+    # Calculate the quadratures of higher order Q and lower order q
+    Q = h * (w1*f1 + w2*f2 + w3*f3 + w4*f4)
+    q = h * (v1*f1 + v2*f2 + v3*f3 + v4*f4)
+
+    # Estimate of the local error and tolerance
+    err = abs(Q - q)
+    tol = acc + eps*abs(Q)
+
+    # If the error is small: Accept the integration; return estimate and error
+    if err < tol:
+        return Q, err
+    #
+    # If not: Split the interval in two and integrate each seperately with a
+    # scaled absolute accuracy goal. Appropriate points are re-used.
+    else:
+        accscale = math.sqrt(2)
+        Ql, errl = roquad24(F, a, a+h/2.0, f1, f2, acc/accscale, eps, nrec+1)
+        Qr, errr = roquad24(F, a+h/2.0, b, f3, f4, acc/accscale, eps, nrec+1)
+        Qtot = Ql + Qr
+        errtot = math.sqrt(errl*errl + errr*errr)
+        return Qtot, errtot
--- 8_integ/mainA.pyx
+++ 8_integ/mainA.pyx
@@ -1,9 +1,19 @@
 # cython: language_level=3
 
 import numpy as np
+import math
 import auxA as adapt
 import sys
 
+# Global variable to count number of function calls
+import globvar
+
+#
+# Test-functions to integrate
+#
+def f1(x):
+    globvar.calls += 1
+    return math.exp(-x**2) * 2.0/math.sqrt(math.pi)
 
 #
 # Main function
@@ -12,3 +22,33 @@ def mainA():
     """
     Test of the ODE routines
     """
+    ##############
+    # Function 1 #
+    ##############
+    # Conditions
+    acc = 1e-3
+    eps = 1e-3
+    a1 = 0
+    b1 = 1
+    
+    # Pretty print!
+    print('\n ** Integrating 2*exp(-x^2)/sqrt(pi) from', a1, 'to', b1, ' ** ')
+    print('Exact solution = erf(1) ~ {0:.15f}'.format(math.erf(1)))
+
+    # Closed quadratures
+    globvar.calls = 0
+    Qc1, errc1 = adapt.rcquad(f1, a1, b1, acc, eps)
+    print('- Integration with closed quadratures:')
+    print('Integrand calls =', globvar.calls)
+    print('Integral        = {0:.15f}'.format(Qc1))
+    print('Error estimate  = {0:.5e}'.format(errc1))
+    print('Actual error    = {0:.5e}'.format(abs(Qc1 -  0.84270079295)))
+
+    # Open quadratures
+    globvar.calls = 0
+    Qo1, erro1 = adapt.roquad(f1, a1, b1, acc, eps)
+    print('- Integration with open quadratures:')
+    print('Integrand calls =', globvar.calls)
+    print('Integral        = {0:.15f}'.format(Qo1))
+    print('Error estimate  = {0:.5e}'.format(erro1))
+    print('Actual error    = {0:.5e}'.format(abs(Qo1 -  0.84270079295)))
