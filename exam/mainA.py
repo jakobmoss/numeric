fe1ac56bdb0317e00d3271d6891a6eea9d41eabf
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-07-01 10:28:26 +0200

    Now with the main in different functions

--- exam/mainA.py
+++ exam/mainA.py
@@ -2,7 +2,7 @@
 # Numerical Methods 2015
 # Examination assignment
 # Jakob Rørsted Mosumgaard
-# Time-stamp: <2015-07-01 09:40:18 moss>
+# Time-stamp: <2015-07-01 10:28:14 moss>
 #
 # Part A
 ###########################################
@@ -20,35 +20,69 @@ import eigen
 
 
 #
-# Run
+# Auxiliary functions for organizing the testing
 #
+def initsys():
+    """
+    Initialize testing matrix and NumPy eigenvalues- and vectors.
+    Prints to stdout.
+    """
+    # Test-matrix
+    A = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2],
+                  [10, 9, 2, 3]], dtype='float')
+    print('Test-matrix: A =\n', A)
 
-# Test-matrix
-A = np.array([[-5, 9, -4, 10], [9, -1, 3, 9], [-4, 3, -6, 2], [10, 9, 2, 3]],
-             dtype='float')
-print('Test-matrix: A =\n', A)
+    # Results from NumPy routine
+    npval, npvec = la.eig(A)
+    print('\nEigenvalues by NumPy :\n', npval)
+    print('Eigenvectors NumPy   :\n', npvec)
 
-# Results from NumPy routine
-npval, npvec = la.eig(A)
-print('\nEigenvalues by NumPy :\n', npval)
-print('Eigenvectors NumPy   :\n', npvec)
+    # Return
+    return A, npval, npvec
 
-# Run own algorithm. Pretty print!
-print('\n\n*************************************************')
-print('** Finding eigenvalues using inverse iteration **')
-print('*************************************************')
 
-# Eigenvalue of leat magnitude
-iters = 10
-print('\nFinding eigenvalue of least magnitude using', iters, 'iterations')
-valmin, vecmin = eigen.inviter(A)
-print('Estimated eigenvalue  :', valmin)
-print('Estimated eigenvector :', vecmin.T)
+def basictest(A, iters, guesses):
+    """
+    Basic test of the inverse iteration algorithm with and without a shift
+    Prints to stdout.
+    """
+    # Pretty print!
+    print('\n\n*************************************************')
+    print('** Finding eigenvalues using inverse iteration **')
+    print('*************************************************')
 
-# Test if different eigenvalues can be found using a shift
-print('\n\n-- Searching for different eigenvalues by introducing a shift')
-for guess in [-13, -7, -1, 12]:
-    val, vec = eigen.inviter(A, iters, guess)
-    print('\nFinding eigenvalue near', guess, 'using', iters, 'iterations...')
-    print('Eigenvalue by inverse iteration  :', val)
-    print('Eigenvector by inverse iteration :', vec.T)
+    # Eigenvalue of leat magnitude
+    iters = 10
+    print('\nFinding eigenvalue of least magnitude using', iters, 'iterations')
+    valmin, vecmin = eigen.inviter(A)
+    print('Estimated eigenvalue  :', valmin)
+    print('Estimated eigenvector :', vecmin.T)
+
+    # Test if different eigenvalues can be found using a shift
+    print('\n\n-- Searching for different eigenvalues by introducing a shift')
+    for guess in guesses:
+        val, vec = eigen.inviter(A, iters, guess)
+        print('\nFinding eigenvalue near', guess, 'using', iters,
+              'iterations...')
+        print('Eigenvalue by inverse iteration  :', val)
+        print('Eigenvector by inverse iteration :', vec.T)
+
+
+#
+# Main of Part A
+#
+def mainA():
+    # Initialize
+    A, npval, npvec = initsys()
+
+    # Basic test of the algorithm
+    Niter = 10
+    eigenguess = [-13, -7, -1, 12]
+    basictest(A, Niter, eigenguess)
+
+
+#
+# If the file is called directly: Run the main!
+#
+if __name__ == '__main__':
+    mainA()
