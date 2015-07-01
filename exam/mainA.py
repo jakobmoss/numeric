da7dbfee534ef1621df4b184d9cdc84b24059dea
Jakob Rørsted Mosumgaard <jakob@phys.au.dk>
2015-07-01 10:53:33 +0200

    Now with argument parsing!

--- exam/mainA.py
+++ exam/mainA.py
@@ -2,7 +2,7 @@
 # Numerical Methods 2015
 # Examination assignment
 # Jakob Rørsted Mosumgaard
-# Time-stamp: <2015-07-01 10:28:14 moss>
+# Time-stamp: <2015-07-01 10:53:00 moss>
 #
 # Part A
 ###########################################
@@ -12,8 +12,12 @@
 #
 
 # General
+import sys
 import numpy as np
 import numpy.linalg as la
+
+# For passing options
+import argparse
 
 # Homemade routines for iterative determination of eigenvalues
 import eigen
@@ -71,18 +75,44 @@ def basictest(A, iters, guesses):
 #
 # Main of Part A
 #
-def mainA():
-    # Initialize
+def mainA(**options):
+    # Get testing-matrix and eigens determined by NumPy
     A, npval, npvec = initsys()
 
     # Basic test of the algorithm
-    Niter = 10
-    eigenguess = [-13, -7, -1, 12]
-    basictest(A, Niter, eigenguess)
+    if options['basic']:
+        Niter = 10
+        eigenguess = [-13, -7, -1, 12]
+        basictest(A, Niter, eigenguess)
 
+    # Test of convergence as a function of iterations
+    elif options['convergence']:
+        print('Option works', file=sys.stderr)
+
+    # No options given
+    else:
+        print('No option given!', file=sys.stderr)
+
+
+#
+# Parsing of arguments to the script
+#
+def main():
+    # Initialize parser
+    parser = argparse.ArgumentParser(description='Inverse iteration method')
+
+    # Add and gather keywords
+    parser.add_argument('--basic', action='store_true',
+                        help='Basic test of the algorithm')
+    parser.add_argument('--convergence', action='store_true',
+                        help='Test of the convergence rate')
+    args = parser.parse_args()
+
+    # Run the real main-function with the parsed options
+    mainA(**vars(args))
 
 #
 # If the file is called directly: Run the main!
 #
 if __name__ == '__main__':
-    mainA()
+    main()
